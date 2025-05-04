1d89860138f5306dbbf02bd622e1fd1fb2637eed
aokpc <181478235+aokpc@users.noreply.github.com>
2025-05-04 20:42:13 +0900

    rm dist

--- server/main.ts
+++ server/main.ts
@@ -1,6 +1,6 @@
 import { Application } from "jsr:@oak/oak/application";
 import { Router } from "jsr:@oak/oak/router";
-import routeStaticFilesFrom from "./util/routeStaticFilesFrom.ts";
+import { routeStaticFilesFrom, routeIndexHTMLFrom } from "./util/routeStaticFilesFrom.ts";
 
 export const app = new Application();
 const router = new Router();
@@ -10,7 +10,9 @@ app.use(routeStaticFilesFrom([
   `${Deno.cwd()}/client/dist`,
   `${Deno.cwd()}/client/public`,
 ]));
-
+app.use(routeIndexHTMLFrom(
+  `${Deno.cwd()}/client/dist/`,
+));
 if (import.meta.main) {
   console.log("Server listening on port http://localhost");
   await app.listen({ port: 80, hostname: "0.0.0.0" });
--- server/util/routeStaticFilesFrom.ts
+++ server/util/routeStaticFilesFrom.ts
@@ -3,7 +3,7 @@ import { Context } from "jsr:@oak/oak/context";
 
 // Configure static site routes so that we can serve
 // the Vite build output and the public folder
-export default function routeStaticFilesFrom(staticPaths: string[]) {
+export function routeStaticFilesFrom(staticPaths: string[]) {
   return async (context: Context<Record<string, object>>, next: Next) => {
     for (const path of staticPaths) {
       try {
@@ -12,6 +12,18 @@ export default function routeStaticFilesFrom(staticPaths: string[]) {
       } catch {
         continue;
       }
+    }
+    await next();
+  };
+}
+
+export function routeIndexHTMLFrom(htmlPath: string) {
+  return async (context: Context<Record<string, object>>, next: Next) => {
+    try {
+      await context.send({ root: htmlPath, path: "index.html" });
+      return;
+    } catch {
+
     }
     await next();
   };
